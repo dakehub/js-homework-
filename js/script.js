0efe243d2aca59a377298c3c2702c0393235d439
Daurenbek <dake.buy@gmail.com>
2022-08-16 17:19:46 +0500

    firstCommit

---
+++ js/script.js
@@ -0,0 +1,71 @@
+/* 11.08.2022*/
+/*домашка 1*/
+
+
+
+/*============================================*/
+/* Домашка 2
+const num1 = Number(prompt());
+const num2 = Number(prompt());
+const num3 = Number(prompt());
+
+if (num1 > num2 && num1 > num3) {
+	alert(num1);
+} else if (num1 < num2 && num2 > num3) {
+	alert(num2);
+} else if (num1 < num3 && num2 < num3) {
+	alert(num3);
+}
+*/
+
+
+/*============================================*/
+
+/* Не доделал домашка 3*/
+/*
+const randomNum = Number(prompt());
+
+const numRandom = Math.round(Math.random() * 3);
+
+let times = (randomNum == numRandom)
+	
+
+for (times = 0; times < 3; times++){
+	let times = (randomNum == numRandom)
+	?
+	alert('Поздравляю ты угадал')
+	:
+	alert('У тебя еще 3 попытки');
+}
+*/
+
+
+/*============================================*/
+/*домашка 4*/
+/* сделал
+const word = prompt("Введите слово");
+for (let i of word) {
+	console.log(i)
+}
+*/
+
+/*============================================*/
+/*домашка 5*/
+/*
+for (i = 50; i < 101; i++){
+	console.log(i)
+}
+*/
+
+
+/*домашка 6*/
+/*  Не смог reverse
+let n1 = prompt('Введите целое число: ');
+let n_list = Array(n1);
+n_list.reverse();
+
+console.log(n_list)
+
+/*домашка 7*/
+
+
